a2fbf878bc9cb5d84efd824af61144b3d8581287
buli <maciek.bulczak@gmail.com>
2025-07-29 12:46:32 +0200

    update logger

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -16,18 +16,18 @@ const Colors = {
 } as const;
 
 const createLogger = () => {
+  const dateFormatter = new Intl.DateTimeFormat("pl-PL", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+
   const formatDate = (): string => {
-    return new Intl.DateTimeFormat("pl-PL", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
-    })
-      .format(new Date())
-      .replace(",", "");
+    return dateFormatter.format(new Date()).replace(",", "");
   };
 
   const getColorForLevel = (level: LogLevel): string => {
